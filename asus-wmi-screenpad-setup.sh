947181a8b1dfb14c74738d2c5f09258154584bf3
yuiiio <atbjyk@protonmail.com>
2023-04-05 21:40:45 +0900

    update to 6.3-rc5

--- asus-wmi-screenpad-setup.sh
+++ asus-wmi-screenpad-setup.sh
@@ -4,7 +4,7 @@ sudo rm -r /usr/src/asus-wmi-1.0
 sudo mkdir /usr/src/asus-wmi-1.0
 sudo cp -r ~/asus-wmi-screenpad/* /usr/src/asus-wmi-1.0/
 cd /usr/src/asus-wmi-1.0/
-sudo sh prepare-for-current-kernel.sh
+#sudo sh prepare-for-current-kernel.sh
 sudo dkms add -m asus-wmi -v 1.0
 
 sudo dkms build -m asus-wmi -v 1.0
